3c62898ae7b51960568b5fb0972881a3f1d47722
emilio0310-guía <emilio.diaz.2805@hotmail.com>
2026-02-24 20:21:02 -0500

    primer commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def request_info():
+    url = 'http://localhost:8080/api/info'
+    response = requests.get(url)
+    return response.json()
+
+def request_sumar(a, b):
+    url = 'http://localhost:8080/api/sumar'
+    data = {
+        'a': a,
+        'b': b
+        }
+    response = requests.post(url, json=data)
+    return response.json()
+
+respuesta = request_sumar(5, 10)
+print(f"Resultado de sumar: {respuesta}")
